7bad498c0b3023da1448f3cc125323a16722d462
amoskarugo <karugoamos@gmail.com>
2024-04-01 12:49:47 +0300

    #

---
+++ JavaScriptCoreConcepts/app.js
@@ -0,0 +1,21 @@
+console.clear()
+
+
+
+function addTwoNumbers(a, b){
+    const sum = a + b;
+
+    return sum;
+}
+let android = ['samsung', 'oppo', 'readme', 'techno']
+let ios = ['iphone 7','iphone 11', 'iphone 12'];
+// push pop shift unshift
+let phones = android.concat(ios)
+// splice slice concat
+
+// splice=> add, remove, replace
+// slice => create subarr or copy
+console.log(phones)
+
+//indexOf lastindexOf, includes, forEach, find, filter, findIndex
+// map reduce, sort , reverse
